8779fe5959e1fff9c19925b8a3f00681ea582285
Arnaud Lenglet <arlenglet@gmail.com>
2018-05-01 14:58:32 +0200

    feat(parser): finish parser that write to file

--- conversation_api.rb
+++ conversation_api.rb
@@ -1,22 +1,37 @@
-require 'intercom'
-
 class ConversationApi
-  attr_reader :intercom, :conversation_parser
+  attr_reader :intercom
 
-  def initialize
-    # You should alwasy store you access token in a environment variable
-    # This ensures you never accidentally expose it in your code
-    @intercom = Intercom::Client.new(token: ENV["PANDA_INTERCOM_ACCESS_TOKEN"])
-    @conversation_parser = ConversationParser.new(intercom)
+  def initialize(intercom)
+    @intercom = intercom
   end
 
-  def get(conversation_id)
+  def get_single_conversation(conversation_id)
     intercom.conversations.find(id: conversation_id)
   end
 
-  def get_all_conversations
+  def get_first_page_of_conversations
     # Get the first page of your conversations
-    conversations = intercom.get("/conversations", "")
+    check_rate_limit do
+      conversations = intercom.get("/conversations", "")
+    end
+  end
+
+  def get_next_page_of_conversations(next_page_url)
+    check_rate_limit do
+      conversations = intercom.get(next_page_url, "")
+    end
+  end
+
+  private
+
+  def check_rate_limit
+    current_rate = yield
+    puts "RATE LIMIT: #{intercom.rate_limit_details[:remaining]}"
+    if intercom.rate_limit_details[:remaining] && intercom.rate_limit_details[:remaining] <= 30
+      sleep 10
+      puts "SLEEPING... zzZZzzZzzz..."
+    end
+    current_rate
   end
 
 end
--- run.rb
+++ run.rb
@@ -1,21 +1,25 @@
 require 'intercom'
+require 'time'
 require './conversation_parser'
 require './conversation_api'
-require './file_writer'
 
+@output_file_name = "#{Time.now.to_i}_panda_intercom.export"
 
 @intercom = Intercom::Client.new(token: ENV["PANDA_INTERCOM_ACCESS_TOKEN"])
-@conversation_parser = ConversationParser.new(@intercom)
-@conversation_api = ConversationApi.new()
-@file_writer = FileWriter.new("test")
+@conversation_parser = ConversationParser.new(@intercom, @output_file_name)
+@conversation_api = ConversationApi.new(@intercom)
+
+
+puts ">>> --- Script started @ #{Time.now}"
 
 # Get all conversations
-# conversations = @conversation_api.get_all_conversations()
-# @conversation_parser.parse_multiple_conversations(conversations)
+@conversation_parser.write_conversations_to_file()
 
 # Get one conversation
-# conversation = @conversation_api.get(1)
+# conversation = @conversation_api.get_single_conversation(1)
 # @conversation_parser.parse_single_conversation(conversation)
 
 # Debug: list app tags to ensure API communication works
-@intercom.tags.all.map {|tag| tag.name }
+# @intercom.tags.all.map {|tag| tag.name }
+
+puts ">>> --- Script ended @ #{Time.now}"
