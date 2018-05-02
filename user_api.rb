107859ed06fdeda512352864b3f2ad22fa663f5d
Arnaud Lenglet <arlenglet@gmail.com>
2018-05-02 17:48:27 +0200

    feat(user_api): create UserApi + #get_user/#get_admin

---
+++ user_api.rb
@@ -0,0 +1,16 @@
+class UserApi
+  attr_reader :intercom
+
+  def initialize(intercom)
+    @intercom = intercom
+  end
+
+  def get_user_by_id(id)
+    intercom.users.find(id: id)
+  end
+
+  def get_admin_by_id(id)
+    intercom.admins.find(id: id)
+  end
+
+end
